a19e530dd74d208fcd83e313401f9602aed0daae
kuba <j.palczewski@gamedia.biz>
2019-01-26 01:54:29 +0100

    Created basic template for CurrentWeatherFragment and background animation

--- app/src/main/java/pl/wroc/ue/weather/fragments/CurrentWeatherFragment.java
+++ app/src/main/java/pl/wroc/ue/weather/fragments/CurrentWeatherFragment.java
@@ -1,17 +1,23 @@
 package pl.wroc.ue.weather.fragments;
 
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import pl.wroc.ue.weather.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CurrentWeatherFragment extends Fragment {
+
+  private RelativeLayout layout;
+  private AnimationDrawable animationDrawable;
 
   public CurrentWeatherFragment() {
   }
@@ -25,4 +31,22 @@ public class CurrentWeatherFragment extends Fragment {
     return inflater.inflate(R.layout.fragment_current_weather, container, false);
   }
 
+  @Override
+  public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    layout = getView().findViewById(R.id.relativeLayout);
+    animationDrawable = (AnimationDrawable) layout.getBackground();
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    animationDrawable.start();
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    animationDrawable.stop();
+  }
 }
